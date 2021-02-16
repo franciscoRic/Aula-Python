5600bb5d8996e36ff259cd86a344c481ab8d5c87
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-02-15 23:19:34 -0400

    Modulos_customizados

--- funcao_com_retorno.py
+++ funcao_com_retorno.py
@@ -39,3 +39,9 @@ print(joaga_moeda())
 
 """
 
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total = total + num
+    return total
---
+++ modulos_customizados.py
@@ -0,0 +1,10 @@
+"""
+Modulos Builtin
+
+
+"""
+#Importando uma função de um modulo expecifico
+from funcao_com_retorno import soma_impares
+
+print(soma_impares([10, 20, 31, 41, 50]))
+
